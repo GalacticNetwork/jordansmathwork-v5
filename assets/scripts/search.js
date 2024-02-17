c715d2f58acdc743457227b640ebfc670e174b8a
GalacticNetwork <114846211+GalacticNetwork@users.noreply.github.com>
2024-02-17 23:44:24 +0000

    idk
    d

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,16 +1,21 @@
-const boxes = document.querySelectorAll('.list');
+function filterGames() {
+  var input, filter, box, games, i, txtValue;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  box = document.getElementsByClassName('box')[0];
+  games = box.getElementsByClassName('link');
 
-function filterBoxes() {
-    const searchTerm = document.getElementById('searchInput').value.toLowerCase();
-
-    boxes.forEach(box => {
-        const boxTitle = box.querySelector('span').textContent.toLowerCase();
-        if (boxTitle.includes(searchTerm)) {
-            box.style.display = 'block';
-        } else {
-            box.style.display = 'none';
-        }
-    });
+  for (i = 0; i < games.length; i++) {
+    txtValue = games[i].innerText || games[i].textContent;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      games[i].style.display = "";
+    } else {
+      games[i].style.display = "none";
+    }
+  }
 }
 
-document.getElementById('searchInput').addEventListener('input', filterBoxes);
+document.getElementById('searchInput').addEventListener('input', filterGames);
+
+
+// Clipped Was Here (;
