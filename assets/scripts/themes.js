1cf53703590cecac222d1187d58581a4901a76d6
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-02-27 22:48:41 +0000

    please

--- assets/scripts/themes.js
+++ assets/scripts/themes.js
@@ -1,12 +1,14 @@
 const themepicker = document.getElementById("themepicker");
+
 if (localStorage.getItem("theme")) {
-  document.body.setAttribute("theme", localStorage.getItem("theme"))
+  document.body.setAttribute("data-theme", localStorage.getItem("theme"));
+  themepicker.value = localStorage.getItem("theme");
 }
 
 themepicker.addEventListener("change", function(e) {
   let theme = e.target.value;
   if (theme !== "select") {
-    document.body.setAttribute('theme', theme)
+    document.body.setAttribute('data-theme', theme);
     localStorage.setItem("theme", theme);
   }
 });
