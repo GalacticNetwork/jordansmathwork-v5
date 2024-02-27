19ff51ca606486364cf00120f4cfdd77d58fdd93
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-02-27 22:53:41 +0000

    gyat

--- assets/scripts/themes.js
+++ assets/scripts/themes.js
@@ -3,6 +3,9 @@ const themepicker = document.getElementById("themepicker");
 if (localStorage.getItem("theme")) {
   document.body.setAttribute("data-theme", localStorage.getItem("theme"));
   themepicker.value = localStorage.getItem("theme");
+} else {
+  document.body.setAttribute("data-theme", "classic"); // Default theme
+  localStorage.setItem("theme", "classic");
 }
 
 themepicker.addEventListener("change", function(e) {
