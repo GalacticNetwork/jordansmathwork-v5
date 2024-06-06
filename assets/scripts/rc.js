0f5722f0827473a2480b1751ed56031cddc5ee79
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-06-06 01:45:32 +0000

    fixing stuff

--- assets/scripts/rc.js
+++ assets/scripts/rc.js
@@ -3,25 +3,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
     contextMenu.id = 'customContextMenu';
     contextMenu.style.position = 'absolute';
     contextMenu.style.display = 'none';
-    contextMenu.style.backgroundColor = 'var(--navbar)';
+    contextMenu.style.backgroundColor = '#ffffff';
     contextMenu.style.border = '1px solid #ccc';
     contextMenu.style.zIndex = '1000';
 
     const aboutBlank = document.createElement('div');
-    aboutBlank.textContent = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z"/></svg> about:blank';
+    aboutBlank.innerHTML = '<i class="fa-solid fa-eye-slash"></i> about:blank';
     aboutBlank.style.padding = '8px';
     aboutBlank.style.cursor = 'pointer';
-    aboutBlank.style.color = 'var(--txtcolor)';
+    aboutBlank.style.color = 'black';
     aboutBlank.onclick = function() {
         aboutBlankFunction();
         contextMenu.style.display = 'none';
     };
 
     const panic = document.createElement('div');
-    panic.textContent = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg> panic';
+    panic.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> panic';
     panic.style.padding = '8px';
     panic.style.cursor = 'pointer';
-    panic.style.color = 'var(--txtcolor)';
+    panic.style.color = 'black';
     panic.onclick = function() {
         panicFunction();
         contextMenu.style.display = 'none';
@@ -43,11 +43,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     function aboutBlankFunction() {
-          let inFrame; try { inFrame = window !== top; } catch (e) { inFrame = true; }
-          if (!inFrame && !navigator.userAgent.includes("Firefox")) { const popup = open("about:blank", "_blank"); if (!popup || popup.closed) { alert("Allow popups and redirects."); } else { const doc = popup.document; const iframe = doc.createElement("iframe"); const style = iframe.style; const link = doc.createElement("link"); const name = localStorage.getItem("name") || "Home"; const icon = localStorage.getItem("icon") || "https://ssl.gstatic.com/classroom/favicon.png"; doc.title = name; link.rel = "icon"; link.href = icon; iframe.src = location.href; style.position = "fixed"; style.top = style.bottom = style.left = style.right = 0; style.border = style.outline = "none"; style.width = style.height = "100%"; doc.head.appendChild(link); doc.body.appendChild(iframe); location.replace("https://classroom.google.com"); } }
+        document.open();
+        document.write('');
+        document.close();
     }
 
     function panicFunction() {
-        location.replace('https://classroom.google.com');
+        location.replace('https://www.google.com');
     }
 });
