10bbddc2eb76b29d143440dc7c24fe9967c91962
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-06-06 02:01:35 +0000

    fixed it again - v5.7.4

--- assets/scripts/rc.js
+++ assets/scripts/rc.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     contextMenu.style.zIndex = '1000';
 
     const aboutBlank = document.createElement('div');
-    aboutBlank.innerHTML = '<i class="fa-solid fa-eye-slash"></i> about:blank';
+    aboutBlank.innerHTML = '<i class="fa-solid fa-eye-slash"></i> History Hider';
     aboutBlank.style.padding = '8px';
     aboutBlank.style.cursor = 'pointer';
     aboutBlank.style.color = 'var(--txtcolor)';
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     };
 
     const panic = document.createElement('div');
-    panic.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> panic';
+    panic.innerHTML = '<i class="fas fa-exclamation-triangle"></i> Panic Button';
     panic.style.padding = '8px';
     panic.style.cursor = 'pointer';
     panic.style.color = 'var(--txtcolor)';
@@ -27,18 +27,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
         contextMenu.style.display = 'none';
     };
 
-    const view = document.createElement('div');
-    view.innerHTML = '<i class="fa-solid fa-users-gear"></i> View All Settings';
-    view.style.padding = '8px';
-    view.style.cursor = 'pointer';
-    view.style.color = 'var(--txtcolor)';
-    view.onclick = function() {
-        viewFunction();
+    const viewAllSettings = document.createElement('div');
+    viewAllSettings.innerHTML = '<i class="fa-solid fa-users-gear"></i> View All Settings';
+    viewAllSettings.style.padding = '8px';
+    viewAllSettings.style.cursor = 'pointer';
+    viewAllSettings.style.color = 'var(--txtcolor)';
+    viewAllSettings.onclick = function() {
+        viewAllSettingsFunction();
         contextMenu.style.display = 'none';
     };
 
     contextMenu.appendChild(aboutBlank);
     contextMenu.appendChild(panic);
+    contextMenu.appendChild(viewAllSettings);
     document.body.appendChild(contextMenu);
 
     document.addEventListener('contextmenu', (event) => {
@@ -53,14 +54,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     function aboutBlankFunction() {
-          let inFrame; try { inFrame = window !== top; } catch (e) { inFrame = true; }
-          if (!inFrame && !navigator.userAgent.includes("Firefox")) { const popup = open("about:blank", "_blank"); if (!popup || popup.closed) { alert("Allow popups and redirects."); } else { const doc = popup.document; const iframe = doc.createElement("iframe"); const style = iframe.style; const link = doc.createElement("link"); const name = localStorage.getItem("name") || "Home"; const icon = localStorage.getItem("icon") || "https://ssl.gstatic.com/classroom/favicon.png"; doc.title = name; link.rel = "icon"; link.href = icon; iframe.src = location.href; style.position = "fixed"; style.top = style.bottom = style.left = style.right = 0; style.border = style.outline = "none"; style.width = style.height = "100%"; doc.head.appendChild(link); doc.body.appendChild(iframe); location.replace("https://classroom.google.com"); } }
+        document.open();
+        document.write('');
+        document.close();
     }
 
     function panicFunction() {
-        location.replace('https://classroom.google.com');
+        location.replace('https://www.google.com');
     }
-    function viewFunction() {
-       location.href='/geography/' 
-    }    
+
+    function viewAllSettingsFunction() {
+        location.href = '/geography/';
+    }
 });
